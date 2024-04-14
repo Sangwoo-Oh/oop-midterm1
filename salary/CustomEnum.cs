febf3bbb2260adfd8b94ce4e5d927fc9470f6544
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-04-14 20:59:20 +0200

    first

---
+++ salary/CustomEnum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TextFile;
+namespace salary
+{
+	public class CustomEnum
+	{
+		public struct Employee
+		{
+			public string name;
+			public double salary;
+
+			public Employee()
+			{
+                name = "";
+                salary = 0;
+            }
+
+			public override string ToString()
+			{
+				return name + " " + salary;
+			}
+		}
+		public enum Status {
+			norm, abnorm
+		}
+
+        private TextFileReader x;
+		public Status st;
+		public Employee e;
+		private Employee curr;
+		private bool end;
+
+		public CustomEnum(String filename)
+		{
+			x = new TextFileReader(filename);
+        }
+
+        public void read()
+		{
+			x.ReadString(out e.name);
+			bool l = x.ReadDouble(out e.salary);
+			st = l ? Status.norm : Status.abnorm;
+        }
+	}
+}
+
---
+++ salary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Linq;
+using static salary.CustomEnum;
+
+namespace salary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            CustomEnum customEnum = new CustomEnum("input.txt");
+
+            // conditional max search
+            bool l = false;
+            double max = 0;
+            Employee elem = new Employee();
+            customEnum.read();
+            while (customEnum.st == Status.norm)
+            {
+                if (!l && customEnum.e.name[0] == 'B')
+                {
+                    l = true;
+                    max = customEnum.e.salary;
+                    elem = customEnum.e;
+                }
+
+                if (l && customEnum.e.name[0] == 'B')
+                {
+                    if (customEnum.e.salary > max)
+                    {
+                        max = customEnum.e.salary;
+                        elem = customEnum.e;
+                    }                         
+                }
+                customEnum.read();
+            }
+
+            if (l)
+            {
+                Console.WriteLine("max: " + elem.ToString());
+            }
+
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            Console.WriteLine("File not found!");
+        }
+    }
+}
+
